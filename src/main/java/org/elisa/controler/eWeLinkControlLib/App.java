9b1c03e3b1ee111bf94385223d37732c1ddc393b
profile\tdimakopoulos <tdimakopoulos@profilesw.com>
2021-11-28 17:21:31 +0200

    updates

--- src/main/java/org/elisa/controler/eWeLinkControlLib/App.java
+++ src/main/java/org/elisa/controler/eWeLinkControlLib/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main( String[] args )
     {
     	LogineWeLink pLogin=new LogineWeLink();
-    	pLogin.Login("eu","dr.thomas.dimakopoulos@gmail.com","cb1312ef");
+    	pLogin.Login("eu","dr.thomas.dimakopoulos@gmail.com","xxxxx");
     	LiveManager pLive=new LiveManager();
     	pLive.setEweLink(pLogin.getEweLink());
     	pLive.StartLiveMonitor();
